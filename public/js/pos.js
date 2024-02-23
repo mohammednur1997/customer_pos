9be77bf8b55ca1271f82ec6f6e34a343eb805e24
mohammednur1997 <azmisoft1997@gmail.com>
2024-02-23 22:54:10 +0300

    dfdf

--- public/js/pos.js
+++ public/js/pos.js
@@ -1528,7 +1528,7 @@ function pos_product_row(variation_id = null, purchase_line_id = null, weighing_
                     //Increment product quantity
                     qty_element = $(this).find('.pos_quantity');
                     var qty = __read_number(qty_element);
-                    __write_number(qty_element, qty + 1);
+                  //  __write_number(qty_element, qty + 1);
                     qty_element.change();
 
                     round_row_to_iraqi_dinnar($(this));
